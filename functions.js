150f384c188755097863385b327fc6cf41c19e7d
kevin-bog <44158857+kevin-bog@users.noreply.github.com>
2021-02-13 16:01:17 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,24 +1,79 @@
-function scrollToDiv() {
-    const firstDiv = document.querySelectorAll('[data-scroll-order]')[0]
-    const secondDiv = document.querySelectorAll('[data-scroll-order]')[1]
+let isScrollUp = false;
+let scrollY = 0;
 
-    document.addEventListener("scroll", (e) => {
-            if (secondDiv.getBoundingClientRect().top > 0) {
-                if (!isScrollUp) {
-                    secondDiv.scrollIntoView();
-                }
-            } else {
-                if (isScrollUp) {
-                    firstDiv.scrollIntoView();
-                }
+var scrollTimeout = null;
+document.addEventListener("scroll", function(e){
+    document.querySelector("body").style.overflowY = "hidden";
+   if (scrollTimeout) clearTimeout(scrollTimeout);
+    scrollTimeout = setTimeout(function(){
+        scrollToDiv(e)
+    },50);
+});
+
+
+const cdSections = document.querySelectorAll("[data-scroll-block]")
+const sectionsSize = [];
+cdSections.forEach(element=>{
+    const yTop = getPosition(element).y;
+    const yBottom =  yTop + getOuterHeight(element);
+    sectionsSize.push({
+        yTop: yTop,
+        yBottom:yBottom
+    })
+
+})
+
+
+function scrollToDiv(){
+    for(let i = 0 ; i < cdSections.length ; i++) {
+        const elmtnY = sectionsSize[i];
+        let yTop = elmtnY.yTop
+        let yBottom = elmtnY.yBottom
+        const userPosition = scrollY;
+
+        if (userPosition -  4  > yTop && userPosition + 4 < yBottom){
+            if (isScrollUp){
+                window.scrollTo({
+                  top: yTop,
+                  behavior: 'smooth'
+                });
+                document.querySelector("body").style.overflowY = "auto";
+            }else {
+                window.scrollTo({
+                  top: yBottom,
+                  behavior: 'smooth'
+                });
+                document.querySelector("body").style.overflowY = "scroll";
             }
+        }else{
+                document.querySelector("body").style.overflowY = "scroll";
         }
-    );
-    let isScrollUp = true;
 
-    window.onscroll = function (e) {
-        isScrollUp = this.oldScroll > this.scrollY;
-        this.oldScroll = this.scrollY;
     }
 }
-scrollToDiv();
+
+window.onscroll = function (e) {
+    isScrollUp = this.oldScroll > this.scrollY;
+    scrollY = this.scrollY;
+    this.oldScroll = this.scrollY;
+};
+
+function getOuterHeight(el) {
+    let elHeight = el.offsetHeight;
+    elHeight += parseInt(window.getComputedStyle(el).getPropertyValue('margin-top'));
+    elHeight += parseInt(window.getComputedStyle(el).getPropertyValue('margin-bottom'));
+    return elHeight
+}
+
+function getPosition(element) {
+    var xPosition = 0;
+    var yPosition = 0;
+
+    while(element) {
+        xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+        yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+        element = element.offsetParent;
+    }
+
+    return { x: xPosition, y: yPosition };
+}
